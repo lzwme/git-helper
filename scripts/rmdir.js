3579e6a4b34cfb73be3f915839907c92982b5f77
renxia <lzwy0820@qq.com>
2022-11-17 17:04:01 +0800

    chore: fix for rmdir scripts

--- scripts/rmdir.js
+++ scripts/rmdir.js
@@ -6,7 +6,7 @@ const start = () => {
   const list = process.argv.slice(2).filter(Boolean).map(d => path.resolve(d));
   list.forEach(dest => {
     if (!fs.existsSync(dest)) return console.log('指定的目录不存在：', dest);
-    fs.rmSync(dir, { recursive: true, force: true });
+    fs.rmSync(dest, { recursive: true, force: true });
     console.log('目录已删除：', dest);
   });
 }
