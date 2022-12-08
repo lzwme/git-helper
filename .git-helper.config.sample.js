bddcae58af55561639e81c79650fec3272caf351
renxia <lzwy0820@qq.com>
2022-12-08 13:48:10 +0800

    docs: update doc for config

--- .git-helper.config.sample.js
+++ .git-helper.config.sample.js
@@ -1,13 +1,20 @@
 /**
- * @type {import('./dist').IConfig}
+ * @type {import('./').IConfig}
  */
  module.exports = {
-  configPath: ".git-helper.config.js",
   debug: false,
   baseDir: process.cwd(),
   commit: {
     messageReg: /^(fix|feat|pref|test|doc):.+/,
     push: true,
     noVerify: false,
+  },
+  run: {
+    cmds: {
+      test: {
+        desc: 'test',
+        list: ['npm run lint', 'npm run test']
+      },
+    },
   },
 };
