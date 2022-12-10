dec93d099d5dd6a11d6a0bde199a3070e7981e43
renxia <lzwy0820@qq.com>
2022-12-11 03:50:13 +0800

    test: update for test

--- src/git-utils.spec.ts
+++ src/git-utils.spec.ts
@@ -1,26 +1,60 @@
 import * as gitUtils from './git-utils';
+import { execSync } from './utils';
 
-test('gitUtils.getGitLogList', () => {
-  const res = gitUtils.getGitLogList(1);
-  expect(res.length).toBe(1);
-  expect(res[0].s.length > 0).toBeTruthy();
-  expect(gitUtils.getGitLogList().length).toBe(1);
-  expect(gitUtils.getGitLogList(3).length).toBe(3);
-});
+const gitLogLine = [
+  `a5840084eae998ee4a33ad091d004b9f856c1140`,
+  `a584008 _-_ ea936e476429e71abae4d2b92b95331414116e15 _-_ ea936e4 _-_ 3013cd5`,
+  `3013cd5a74bbfbab88cc9a0d9adbed745aee80d8 _-_ 2022-11-20 18:56:29 +0800 _-_ 2022-11-20 18:56:29 +0800`,
+  `renxia _-_ no@lzw.me _-_ no@lzw.me`,
+  `chore: update dependencies`,
+  `2 days ago`,
+  `2 days ago`,
+]
+  .join(' _-_ ')
+  .trim();
+const execSyncFn = execSync as jest.Mock;
 
-test('gitUtils.getHeadCommitId', () => {
-  expect(typeof gitUtils.getHeadCommitId() === 'string').toBeTruthy();
-});
+jest.mock(
+  './utils.js',
+  jest.fn(() => ({
+    execSync: jest.fn(),
+  }))
+);
 
-test('gitUtils.getHeadBranch', () => {
-  expect(typeof gitUtils.getHeadBranch() === 'string').toBeTruthy();
-});
+describe('git-utils', () => {
+  test('gitUtils.getGitLogList', () => {
+    execSyncFn.mockReturnValueOnce(gitLogLine);
+    const res = gitUtils.getGitLogList(1);
+    expect(res.length).toBe(1);
+    expect(res[0].s.length > 0).toBeTruthy();
 
-test('gitUtils.getHeadDiffFileList', () => {
-  expect(Array.isArray(gitUtils.getHeadDiffFileList())).toBeTruthy();
-  expect(gitUtils.getHeadDiffFileList(1).length > 0).toBeTruthy();
-});
+    execSyncFn.mockReturnValueOnce(gitLogLine);
+    expect(gitUtils.getGitLogList().length).toBe(1);
 
-test('gitUtils.getUserEmail', () => {
-  expect(gitUtils.getUserEmail().length > 0).toBeTruthy();
+    execSyncFn.mockImplementationOnce(() => [gitLogLine, gitLogLine, gitLogLine].join('\n'));
+    expect(gitUtils.getGitLogList(3).length).toBe(3);
+  });
+
+  test('gitUtils.getHeadCommitId', () => {
+    execSyncFn.mockReturnValueOnce('aaaaaaaa');
+    expect(typeof gitUtils.getHeadCommitId() === 'string').toBeTruthy();
+  });
+
+  test('gitUtils.getHeadBranch', () => {
+    execSyncFn.mockReturnValueOnce('main');
+    expect(typeof gitUtils.getHeadBranch() === 'string').toBeTruthy();
+  });
+
+  test('gitUtils.getHeadDiffFileList', () => {
+    execSyncFn.mockReturnValueOnce('123.js\n123.ts');
+    expect(Array.isArray(gitUtils.getHeadDiffFileList())).toBeTruthy();
+
+    execSyncFn.mockReturnValueOnce('abc.ts');
+    expect(gitUtils.getHeadDiffFileList(1).length > 0).toBeTruthy();
+  });
+
+  test('gitUtils.getUserEmail', () => {
+    execSyncFn.mockReturnValueOnce('a@lzw.me');
+    expect(gitUtils.getUserEmail().length > 0).toBeTruthy();
+  });
 });
